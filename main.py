546c6098f08606856bcee76d55f569a0b997b783
KyNaOo <kevan122.wu@gmail.com>
2024-01-04 22:52:54 +0100

    feat:variables

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+print("Welcome to my Tic Tac Toe Game")
+print("")
+print("--------------------------------")
+print("")
+
+mapPositions = [1,2,3,4,5,6,7,8,9]
+MapRows = [[1,2,3], [4,5,6], [7,8,9]]
+rows = 3
+col = 3
+
